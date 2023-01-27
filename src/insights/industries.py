88d6dd32671ffedb74a936ea46ce39a5f3122db4
Clever Júnior <60274873+clever-junior@users.noreply.github.com>
2023-01-27 14:16:57 -0400

    fix: fix lint issues on src/insights/industries.py

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,15 +1,6 @@
-import sys
 from typing import Dict, List
 
 from src.insights.jobs import read
-
-sys.path.insert(
-    0,
-    "C:/Users/clever-junior/OneDrive/Documentos/Code/Trybe/Computer-Science/sd-021-a-project-job-insights/src/insights/jobs.py",
-)
-
-
-path = "C:/Users/clever-junior/OneDrive/Documentos/Code/Trybe/Computer-Science/sd-021-a-project-job-insights/data/jobs.csv"
 
 
 def get_unique_industries(path: str) -> List[str]:
