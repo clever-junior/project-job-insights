de61f17722a7a2ad7b62db81788d7b7120b1f555
Clever Júnior <60274873+clever-junior@users.noreply.github.com>
2023-02-01 00:28:57 -0400

    refactor: reduce the complexity of requisites 8 and 9

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,11 +1,6 @@
-# import sys
 from typing import Dict, List, Union
 
 from src.insights.jobs import read
-
-# sys.path.append(
-#     "C:/Users/clever-junior/OneDrive/Documentos/Code/Trybe/Computer-Science/sd-021-a-project-job-insights"
-# )
 
 
 def get_max_salary(path: str) -> int:
@@ -34,39 +29,18 @@ def get_min_salary(path: str) -> int:
     return min(salaries_list)
 
 
-invalid_job = {"max_salary": 0, "min_salary": 20}
-
-job = {"max_salary": 10000, "min_salary": 200}
-
-salary = 1000
-
-
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
     try:
-        if callable(salary):
-            raise ValueError
         min_salary = int(job["min_salary"])
         max_salary = int(job["max_salary"])
-        int_salary = int(salary)
-        if min_salary > max_salary:
+
+        if callable(salary) or None in job.values() or min_salary > max_salary:
             raise ValueError
-        return int_salary >= min_salary and int_salary <= max_salary
-    except KeyError:
-        raise ValueError
+        return int(salary) in range(min_salary, max_salary)
     except TypeError:
         raise ValueError
-
-
-jobs = [
-    {"max_salary": 0, "min_salary": 10},
-    {"max_salary": 10, "min_salary": 100},
-    {"max_salary": 10000, "min_salary": 200},
-    {"max_salary": 15000, "min_salary": 0},
-    {"max_salary": 1500, "min_salary": 0},
-    {"max_salary": -1, "min_salary": 10},
-]
-
-salaries = [0, 1, 5, 1000, 2000, -1, -2, None, "", [], {}, lambda: 1]
+    except KeyError:
+        raise ValueError
 
 
 def filter_by_salary_range(
@@ -79,9 +53,6 @@ def filter_by_salary_range(
             verify = matches_salary_range(job_dict, salary)
         except ValueError:
             verify = False
-        except TypeError:
-            verify = False
-            print(verify)
         if verify:
             filteredJobs.append(job_dict)
 
