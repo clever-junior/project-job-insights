7547f3fc18d9ccc36e1bd88e797a1b187f1a92da
Clever Júnior <60274873+clever-junior@users.noreply.github.com>
2023-02-08 23:40:57 -0400

    feat: add requisite 12

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    result = read_brazilian_file(path)
+    result_keys = result[0].keys()
+
+    assert "title" in result_keys
+    assert "salary" in result_keys
+    assert "type" in result_keys
