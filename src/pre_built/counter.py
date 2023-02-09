b7ba14092f8073422b1dc0685d85c036d73e702b
Clever Júnior <60274873+clever-junior@users.noreply.github.com>
2023-02-08 23:05:44 -0400

    feat: add requisite 13

--- src/pre_built/counter.py
+++ src/pre_built/counter.py
@@ -1,5 +1,10 @@
+# path = "C:/Users/clever-junior/OneDrive/Documentos
+# /work/Code/Trybe/Computer-Science/sd-021-a-project-job-insights/data/jobs.csv"
+
+
 def count_ocurrences(path: str, word: str) -> int:
-    file = open(path, "r")
-    read_data = file.read()
-    word_count = read_data.lower().count(word.lower())
-    return word_count
+    # file = open(path, "r")
+    # read_data = file.read()
+    # word_count = read_data.lower().count(word.lower())
+    # return word_count
+    pass
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+    word = "developer"
+    with pytest.raises(UnicodeDecodeError):
+        count_ocurrences(path, word)
